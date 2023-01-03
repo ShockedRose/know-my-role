9fdee14d33d82b037eea3bdde2dd1b1a8e4abcd5
Miguel R <miguel.rosa@devsu.com>
2023-01-03 12:00:12 -0500

    feat: button

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,23 @@
-Array.from(document.querySelectorAll('.section__button')).forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        toggleSection(e.target.classList[2].match(/--(\w+)/)[1])
-    })
-
-
-})
+Array.from(document.querySelectorAll(".section__button")).forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		toggleSection(e.target.classList[2].match(/--(\w+)/)[1]);
+	});
+});
 
 function toggleSection(sectionName) {
-    
-    const toToggleOn = Array.from(document.querySelectorAll(`.section__textbox--${sectionName}`))
-    const toToggleOff = Array.from(document.querySelectorAll(`.section__textbox:not(.section__textbox--inactive)`))
+	const toToggleOn = Array.from(
+		document.querySelectorAll(`.section__textbox--${sectionName}`)
+	);
+	const toToggleOff = Array.from(
+		document.querySelectorAll(
+			`.section__textbox:not(.section__textbox--inactive)`
+		)
+	);
 
-
-    toToggleOff.forEach(btn => {
-        btn.classList.toggle('section__textbox--inactive')
-    })
-    toToggleOn.forEach(btn => {
-        btn.classList.toggle('section__textbox--inactive')
-    })
-
-
-
+	toToggleOff.forEach((btn) => {
+		btn.classList.toggle("section__textbox--inactive");
+	});
+	toToggleOn.forEach((btn) => {
+		btn.classList.toggle("section__textbox--inactive");
+	});
 }
